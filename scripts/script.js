3f7b50005603fa508472e1a78f4262a31daf535e
Sugoto Basu <60142374+Sugoto@users.noreply.github.com>
2022-02-02 17:42:38 +0530

    Added menu click-events

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,40 @@
+// ====== Context Menu click-events ======
+
+const contextMenu = document.querySelector(".wrapper"),
+  shareMenu = contextMenu.querySelector(".share-menu");
+
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+
+  let x = e.offsetX,
+    y = e.offsetY,
+    winWidth = window.innerWidth,
+    winHeight = window.innerHeight,
+    cmWidth = contextMenu.offsetWidth,
+    cmHeight = contextMenu.offsetHeight;
+
+  if (x > winWidth - cmWidth - shareMenu.offsetWidth) {
+    shareMenu.style.left = "-200px";
+  } else {
+    shareMenu.style.left = "";
+    shareMenu.style.right = "-200px";
+  }
+
+  x = x > winWidth - cmWidth ? winWidth - cmWidth : x;
+  y = y > winHeight - cmHeight ? winHeight - cmHeight : y;
+
+  contextMenu.style.left = `${x}px`;
+  contextMenu.style.top = `${y}px`;
+  contextMenu.style.visibility = "visible";
+});
+
+document.addEventListener(
+  "click",
+  () => (contextMenu.style.visibility = "hidden")
+);
+
+// ====== Download progress ======
+
 let button = document.querySelector(".dl");
 
 button.addEventListener("click", () => {
